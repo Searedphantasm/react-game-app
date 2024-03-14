c77207c9256a9ed868c3a616879146f6d590d31f
Searedphantasm <searedphantasm@gmail.com>
2024-03-15 00:24:00 +0330

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  // AfterRender
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
